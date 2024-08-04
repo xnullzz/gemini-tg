ab449f8f35366c102eac8678ac8631e2c6191353
xnullzz <xnullzz@xnullz.ddns.net>
2024-08-04 03:43:35 +0300

    Changes

--- gemini_api.py
+++ gemini_api.py
@@ -1,11 +1,19 @@
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 class GeminiAPI:
     def __init__(self, api_key, model="gemini-1.5-pro"):  # Replace ::MODEL_NAME:: with the actual model name
         genai.configure(api_key=api_key)
         self.model = model
+        self.temperature = 0.7
+        self.max_output_tokens = 8192
 
     def generate_text(self, prompt):
         model_selected = genai.GenerativeModel(self.model)
-        response = model_selected.generate_content(prompt)
+        response = model_selected.generate_content(prompt, safety_settings={
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_NONE, } )
         return response.result
