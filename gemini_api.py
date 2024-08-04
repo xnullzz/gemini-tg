c9a1be93516a59776d80b7575a11cc4871157e7d
xnullzz <xnullzz@xnullz.ddns.net>
2024-08-04 04:13:42 +0300

    Changes

--- gemini_api.py
+++ gemini_api.py
@@ -13,7 +13,7 @@ class GeminiAPI:
             HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
             HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
             HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-            HarmCategory.HARM_CATEGORY_UNSPECIFIED: HarmBlockThreshold.BLOCK_ONLY_HIGH, }
+            }
 
 
     def generate_text(self, prompt):
