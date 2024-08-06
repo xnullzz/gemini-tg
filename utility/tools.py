08119234f7899234c939e2a6df9090c29d3076e2
xnullzz <xnullzz@xnullz.ddns.net>
2024-08-07 02:05:11 +0300

    Changes

--- utility/tools.py
+++ utility/tools.py
@@ -1,11 +1,10 @@
 import re
 
 def escape_markdown(text):
-    # First, replace literal '\n' with actual newline characters
-    text = text.replace('\\n', '\n')
-
-    # Escape Markdown special characters except for newline characters
+    
     markdown_chars = r'[\*_\[\]()~`>#\+\-=|{}\.!]'
     escaped_text = re.sub(markdown_chars, lambda m: '\\' + m.group(0), text)
+
+    escaped_text = escaped_text.replace('\\n', '\n')
 
     return escaped_text
