66af7802f60b17793cd37734ef946108e89d62ec
xnullzz <xnullzz@xnullz.ddns.net>
2024-08-07 04:28:15 +0300

    Changes

--- utility/decorators.py
+++ utility/decorators.py
@@ -4,13 +4,13 @@ def authorized_only(allowed_usernames):
     """Decorator to check if the user is authorized."""
 
     def decorator(func):
-        async def wrapper(message: Message):
+        async def wrapper(message: Message, *args, **kwargs):
             username = message.from_user.username
             if username in allowed_usernames:
-                return await func(message)
+                return await func(message, *args, **kwargs)
             else:
-                # Access the bot instance from the decorated function
-                bot = func.__self__  
+                # Access the bot instance from the decorated function's arguments
+                bot = args[0]  # Assuming the bot instance is the first argument
                 await bot.reply_to(message, "You are not authorized to use this bot.")
         return wrapper
     return decorator
